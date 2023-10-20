2a82073de908089eb2c15643eaddb46753304e58
SiddharthVarde22 <21stargames@gmail.com>
2023-10-20 11:26:29 +0530

    Had to remove Feature2d package, & Added monkey drag and drop functionalities

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform monkeyImageRectTransform;
+        private Vector3 initialPosition;
+        private Vector3 initialAnchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -22,6 +25,36 @@ namespace ServiceLocator.UI
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            monkeyImageRectTransform = GetComponent<RectTransform>();
+            initialPosition = monkeyImageRectTransform.anchoredPosition;
+            initialAnchoredPosition = monkeyImageRectTransform.anchoredPosition;
+
+        }
+        public void OnDrag(PointerEventData eventData)
+        {
+            monkeyImageRectTransform.position = eventData.position;
+
+            owner.MonkeyDraggedAt(eventData.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkeyPosition();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkeyPosition()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+            monkeyImageRectTransform.position = initialPosition;
+            monkeyImageRectTransform.anchoredPosition = initialAnchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.6f);
         }
     }
 }
